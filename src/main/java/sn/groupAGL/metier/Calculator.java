292282a964037aee48c1b1f5945bd0d2bf3c5096
Hama10 <mballos1005@gmail.com>
2020-11-28 16:44:25 +0000

    first commit to initializ projetAGL...

---
+++ src/main/java/sn/groupAGL/metier/Calculator.java
@@ -0,0 +1,50 @@
+package sn.groupAGL.metier;
+
+public class Calculator {
+    public int sum( int a, int b ) {
+
+        return 0;
+    }
+
+    public int minus( int a, int b ) {
+
+        return 0;
+    }
+
+    public int divide( int a, int b ) throws Exception {
+
+        return 0;
+    }
+
+    public int multiply( int a, int b ) {
+
+        return 0;
+    }
+
+    public int min( int a, int b ) {
+
+        return 0;
+    }
+
+    public int max( int a, int b ) {
+
+        return 0;
+    }
+
+    /*
+     * Retourne le plus petit élément d'une liste
+     */
+    public int minElement( int[] list ) {
+
+        return 0;
+    }
+
+    /*
+     * Retourne le plus grand élément d'une liste
+     */
+    public int maxElement( int[] list ) {
+
+        return 0;
+    }
+
+}
